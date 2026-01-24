c1a81f0662b22df1393d0f24bd74afa486909dfe
BrayanReis <102810236+oCriativoBrayan@users.noreply.github.com>
2026-01-23 23:26:38 -0300

    Dia 04 - Realiza upload de imagem com allowed valido

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -1,20 +1,45 @@
+import os
 from flask import Blueprint, request, jsonify
+from werkzeug.utils import secure_filename
 
+#define o blueprint de usuários
 user_bp = Blueprint('user_bp', __name__)
 
+#pasta para salvar uploads
+UPLOAD_FOLDER = 'uploads/'
+
+#tipos de arquivos permitidos
+ALLOWED_EXTENSIONS = {'png', 'jpg', 'jpeg'}
+
+#valida se o arquivo tem uma extensão permitida
+def allowed_file(filename):
+    return '.' in filename and \
+           filename.rsplit('.', 1)[1].lower() in ALLOWED_EXTENSIONS
+
 @user_bp.route('/users', methods=['POST'])
-def upload_test():
+def upload_user():
 
-    print("FORM:", request.form)
-    print("FILES:", request.files)
-
+    #pega o arquivo de imagem do request
     image = request.files.get('image')
 
-    if image is None:
-        return jsonify({"error": "imagem não chegou"}), 400
+    #verifica se imagem não está vazia  
+    if image is None or image.filename == '':
+        return jsonify({"Error": "imagem obrigatória"}), 400
+
+    #verifica se o formato da imagem é permitido
+    if not allowed_file(image.filename):
+        return jsonify({"Error": "formato de imagem inválido"}), 400    
+    
+    #garante que o nome do arquivo é seguro
+    filename = secure_filename(image.filename)
+
+    #define o caminho completo para salvar a imagem
+    path = os.path.join(UPLOAD_FOLDER, filename)
+
+    #salva a imagem na pasta de uploads
+    image.save(path)
 
     return jsonify({
         "success": True,
-        "filename": image.filename
+        "filename": filename
     }), 200
-

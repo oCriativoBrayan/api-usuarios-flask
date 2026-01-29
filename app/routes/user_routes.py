d21d7a4b667092ac1fe3c71310328cf4fc342013
BrayanReis <102810236+oCriativoBrayan@users.noreply.github.com>
2026-01-28 23:54:44 -0300

    Dia 8 - CRUD usando PATCH, PUT, DELETE

--- app/routes/user_routes.py
+++ app/routes/user_routes.py
@@ -38,28 +38,46 @@ def create_user():
 
     return jsonify(user.to_dict()), 201
 
-@user_bp.route('/users/<int:user_id>', methods=['PUT'])
+@user_bp.route('/users/<int:user_id>', methods=['PATCH'])
 def update_user(user_id):
     user = User.query.get(user_id)
 
     if not user:
         return jsonify({"error": "usuário não encontrado"}), 404
 
-    name = request.form.get('name')
-    email = request.form.get('email')
+    data = request.form
+
+    if 'name' in data:
+        user.name = data.get('name')
+
+    if 'email' in data:
+        user.email = data.get('email')
+
     image = request.files.get('image')
-
-    if not name or not email:
-        return jsonify({"error": "Nome e email são obrigatórios"}), 400
-
-    filename = None
     if image:
-        filename = image.filename
-        image.save(f'uploads/{filename}')
+        user.image = image.filename
+        image.save(f'uploads/{image.filename}')
 
-    user.name = name
-    user.email = email
-    user.image = filename
+    db.session.commit()
+
+    return jsonify(user.to_dict()), 200
+
+
+
+@user_bp.route('/users/<int:user_id>', methods=['PUT'])
+def replace_user(user_id):
+    user = User.query.get(user_id)
+
+    if not user:
+        return jsonify({"error": "usuário não encontrado"}), 404
+    
+    data = request.form
+
+    if not data.get('name') or not data.get('email'):
+        return jsonify({"error": "Nome e email são obrigatórios"}), 400
+    
+    user.name =  data.get('name')
+    user.email = data.get('email')
 
     db.session.commit()
 

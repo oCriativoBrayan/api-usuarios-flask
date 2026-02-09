0dcedd07938dd0198920e2dc1420008cda64b21f
BrayanReis <102810236+oCriativoBrayan@users.noreply.github.com>
2026-02-08 21:33:39 -0300

    Dia 19 - teste de factory, erro nome funções e dependências

--- main.py
+++ main.py
@@ -1,58 +1,11 @@
 from flask import Flask, request, jsonify
-from app.error import register_error_handlers
-from app.routes.user_routes import user_bp
-from app.logger import setup_logger
-from app.services.user_services import _test_math, create_user
-
-
-import time
-
-app = Flask(__name__)
-
-logger = setup_logger()
-app.logger.handlers = logger.handlers
-app.logger.setLevel(logger.level)
-
-register_error_handlers(app)  
-
-app.register_blueprint(user_bp, url_prefix="/users")
-
-@app.route("/_test_users", methods=["POST"])
-def test_create_user():
-    try:
-        user = create_user(request.json or {})
-        return jsonify(user=user), 201
-    except ValueError as e:
-        return jsonify(error=str(e)), 400
-
-@app.route("/test_math")
-def multiply_route():
-    return jsonify(result=_test_math(8, 2)), 200
-
-@app.before_request
-def start_time():
-    request.start_time = time.time()
+from app.services.user_services import _test_math, create_user #erro por nao importa create_user aqui
 
 def create_app():
-    return "ok"
+    app = Flask(__name__)
 
+    @app.route("/_test_math")
+    def multiply_route():
+        return jsonify(result=_test_math(8, 2)), 200
 
-@app.after_request
-def log_request(response):
-    duration = time.time() - request.start_time
-
-    app.logger.info(
-        f"{request.method} {request.path}"
-        f"{response.status_code}"
-        f"{duration:.3f}s"
-    )
-
-    return response
-
-@app.route("/ping")
-def ping():
-    return {"ok": True}
-
-
-if __name__ == '__main__':
-    app.run(debug=True)
+    return app
--- test/test_main.py
+++ test/test_main.py
@@ -4,54 +4,21 @@ import os
 # Ajuste de path para projeto sem estrutura formal
 sys.path.append(os.path.dirname(os.path.dirname(__file__)))
 
-from main import app, _test_math #chama funcao math
+from main import create_app #chama para test factory
+from app.services.user_services import _test_math #chama funcao teste calculo
 
-#realiza test de usuario valido, inserindo usuario manualmente
-def test_create_user_success():
-    client = app.test_client()
-
-    response = client.post(
-        "/_test_users",
-        json={
-            "name": "Test",
-            "email": "test@example.com"
-        }
-    )
-
-    assert response.status_code == 201
-    assert response.json["user"]["name"] == "Test"
-    assert response.json["user"]["email"] == "test@example.com"
-
-#test cadastro do usuario, com dados invalidos
-def test_create_user_invalid_data():
-    client = app.test_client()
-
-    response = client.post(
-        "/_test_users",
-        json={
-            "name": "Marcos"
-        }
-    )
-
-    assert response.status_code == 400
-    assert "error" in response.json
-
-
-
-def test_multiply_route():
+#teste de calculo
+def test_math():
     assert _test_math(8, 2) == 16
 
-def test_math_route():
+#teste da factory
+def test_multiply_route():
+    app = create_app()
     client = app.test_client()
-    response = client.get("/test_math")
+
+    response = client.get("/_test_math")
 
     assert response.status_code == 200
     assert response.json["result"] == 16
-
-
-def test_ping():
-    client = app.test_client()
-    response = client.get("/ping")
-    assert response.status_code == 200
 
 

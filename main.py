94cddd6e57b105314f02cad3447cc087af45e63a
BrayanReis <102810236+oCriativoBrayan@users.noreply.github.com>
2026-02-09 23:38:42 -0300

    Dia 20 - Estudo fixture e comentario

--- main.py
+++ main.py
@@ -1,12 +1,16 @@
 from flask import Flask, jsonify
 
+#define uma funcao
 def create_app():
+    #inicia um app
     app = Flask(__name__)
-
+    
+    #cria um route /ping
     @app.route("/ping")
     def ping():
         return "pong"
 
+    #criaa um route /math
     @app.route("/math")
     def math():
         return jsonify(result=8)
--- test/test_routes.py
+++ test/test_routes.py
@@ -3,7 +3,9 @@ def test_ping(client):
     assert response.data == b"pong"
 
 def test_math(client):
+    #instancia client
     response = client.get("/math")
-
+    #teste retorno status
     assert response.status_code == 200
+    #test retorno resultado
     assert response.json["result"] == 8

5223cf55d0ed07900c4778fc324793e81998e78d
BrayanReis <102810236+oCriativoBrayan@users.noreply.github.com>
2026-02-11 23:53:52 -0300

    23 - Teste com mock

--- conftest.py
+++ conftest.py
@@ -13,16 +13,22 @@ def client(app):
     #retorno chamando
     return app.test_client()
 
+#cria um fixture (automatizada)
 @pytest.fixture
+#define um funcao
 def app_with_error():
+    #cria um app
     app = create_app()
 
+    #intancia um route
     @app.route("/erro500")
     def error500():
+        #invoca um excecao
         raise Exception("simulated")
-    
+    #retorna o app
     return app
 
+#cria um funcao erro 500, recebendo app da fixture
 def test_500_route(app_with_error):
     client = app_with_error.test_client()
     response = client.get("/erro500")
